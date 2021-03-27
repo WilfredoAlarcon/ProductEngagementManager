abc2b273751de836b4c44f354a570fbf17e468f7
Wilfredo Alarcón <wilfredo.alarcon.38@outlook.com>
2021-03-27 05:14:17 -0500

    Add project files.

---
+++ PEM/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PEM.Models;
+
+namespace PEM.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Loggin()
+        {
+            return View("../Authentication/Loggin");
+        }
+
+        public IActionResult SignIn()
+        {
+            return View("../Authentication/Registration");
+        }
+
+        public IActionResult RecoverPwd() 
+        {
+            return View("../Authentication/RecoverPw");
+        }
+    }
+}
---
+++ PEM/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PEM.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
